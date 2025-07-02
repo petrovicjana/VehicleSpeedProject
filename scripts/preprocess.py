7ad23fe4ee16a3aa29a7373efc7f3cb46913059c
petrovicjana <jance.petrovic2003@gmail.com>
2025-07-02 18:52:26 +0200

    Updated notebooks

---
+++ scripts/preprocess.py
@@ -0,0 +1,9 @@
+import cv2
+
+def get_video_info(video_path):
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    cap.release()
+    return fps, width, height
